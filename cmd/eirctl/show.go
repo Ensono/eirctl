a2bca26a0f24cc8062f32d4065a8e7cd55005531
dnitsch <nitschneiderd@gmail.com>
2025-04-28 19:45:44 +0100

    Feat: context and task re-useable blocks (#20)
    * fix: add re-use folder
    
    * fix: use TF_DIR
    
    * fix: add example
    
    * fix: add additional volume args
    
    * fix: add tests and docs around imports
    +semver: Feature
    +semver: feat
    +semver: feature

--- cmd/eirctl/show.go
+++ cmd/eirctl/show.go
@@ -4,10 +4,57 @@ import (
 	"fmt"
 	"html/template"
 
+	"github.com/Ensono/eirctl/internal/utils"
+	"github.com/Ensono/eirctl/runner"
 	"github.com/spf13/cobra"
 )
 
-var showTmpl = `
+type showContext struct {
+	Name      string
+	Container *runner.ContainerContext
+	Volumes   []string
+	Quote     string
+	Env       map[string]any
+	Vars      map[string]any
+	Envfile   *utils.Envfile
+}
+
+var showContextTmpl = `
+Name: {{ .Name -}}
+{{ if .Container }}
+Image: {{ .Container.Image }}
+Volumes: {{ .Volumes }}
+{{- end }}
+Quote: {{ .Quote }}
+Env: {{ .Env }}
+Variables: {{ .Vars }}
+{{ if .Envfile }}
+Envfile: {{ .Envfile }}
+{{- end }}
+`
+
+type pipelineShow struct {
+	Name      string
+	Generator map[string]any
+	Env       map[string]string
+	Envfile   *utils.Envfile
+}
+
+var showPipelineTmpl = `
+Name: {{ .Name }}
+Env: {{ .Env }}
+CI metadata: {{ .Generator }} 
+{{ if .Envfile }}
+Envfile: {{ .Envfile }}
+{{- end }}
+
+---
+NOTE: to see the nodes of this pipeline run: 
+---
+eirctl graph {{ .Name }}
+`
+
+var showTaskTmpl = `
   Name: {{ .Name -}}
 {{ if .Description }}
   Description: {{ .Description }}
@@ -29,9 +76,9 @@ var showTmpl = `
 func newShowCmd(rootCmd *EirCtlCmd) {
 
 	showCmd := &cobra.Command{
-		Use:     "show",
+		Use:     "show <task|pipeline|context>",
 		Aliases: []string{},
-		Short:   `shows task's details`,
+		Short:   `Shows task, pipeline or context  details, useful for imported configs via remote files/URLs.`,
 		Args:    cobra.RangeArgs(1, 1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			conf, err := rootCmd.initConfig()
@@ -39,10 +86,32 @@ func newShowCmd(rootCmd *EirCtlCmd) {
 				return err
 			}
 
-			t := conf.Tasks[args[0]]
-			if t != nil {
-				tmpl := template.Must(template.New("show").Parse(showTmpl))
-				return tmpl.Execute(rootCmd.ChannelOut, t)
+			taskDef, pipelineDef, contextDef := conf.Tasks[args[0]], conf.Pipelines[args[0]], conf.Contexts[args[0]]
+			if taskDef != nil {
+				tmpl := template.Must(template.New("show-task").Parse(showTaskTmpl))
+				return tmpl.Execute(rootCmd.ChannelOut, taskDef)
+			}
+			if pipelineDef != nil {
+				sp := pipelineShow{Name: pipelineDef.Name(),
+					Env:       pipelineDef.Env,
+					Envfile:   pipelineDef.EnvFile,
+					Generator: pipelineDef.Generator}
+				tmpl := template.Must(template.New("show-pipeline").Parse(showPipelineTmpl))
+				return tmpl.Execute(rootCmd.ChannelOut, sp)
+			}
+			if contextDef != nil {
+				sc := showContext{Name: args[0], Volumes: []string{},
+					Quote: contextDef.Quote, Env: contextDef.Env.Map(),
+					Vars: contextDef.Variables.Map()}
+				cc := contextDef.Container()
+				if cc != nil {
+					sc.Container = cc
+					for vol := range cc.Volumes() {
+						sc.Volumes = append(sc.Volumes, vol)
+					}
+				}
+				tmpl := template.Must(template.New("show-context").Parse(showContextTmpl))
+				return tmpl.Execute(rootCmd.ChannelOut, sc)
 			}
 			return fmt.Errorf("%s. %w", args[0], ErrIncorrectPipelineTaskArg)
 		},
