5513962662d9b74ec76e588f825a5541d82cd556
dnitsch <nitschneiderd@gmail.com>
2025-04-28 19:39:30 +0100

    fix: add tests and docs around imports

--- cmd/eirctl/show_test.go
+++ cmd/eirctl/show_test.go
@@ -18,12 +18,28 @@ func Test_showCommand(t *testing.T) {
 			errored: true,
 		})
 	})
-	t.Run("succeeds on args", func(t *testing.T) {
+	t.Run("task show succeeds", func(t *testing.T) {
 		os.Setenv("EIRCTL_CONFIG_FILE", "testdata/graph.yaml")
 		defer os.Unsetenv("EIRCTL_CONFIG_FILE")
 		cmdRunTestHelper(t, &cmdRunTestInput{
 			args:   []string{"-c", "testdata/graph.yaml", "show", "graph:task1"},
 			output: []string{"Name: graph:task1", "echo &#39;hello, world!&#39"},
+		})
+	})
+	t.Run("pipeline show succeeds args", func(t *testing.T) {
+		os.Setenv("EIRCTL_CONFIG_FILE", "testdata/graph.yaml")
+		defer os.Unsetenv("EIRCTL_CONFIG_FILE")
+		cmdRunTestHelper(t, &cmdRunTestInput{
+			args:   []string{"-c", "testdata/graph.yaml", "show", "graph:pipeline1"},
+			output: []string{"Name: graph:pipeline1"},
+		})
+	})
+	t.Run("context show succeeds args", func(t *testing.T) {
+		os.Setenv("EIRCTL_CONFIG_FILE", "testdata/graph.yaml")
+		defer os.Unsetenv("EIRCTL_CONFIG_FILE")
+		cmdRunTestHelper(t, &cmdRunTestInput{
+			args:   []string{"-c", "testdata/graph.yaml", "show", "foo"},
+			output: []string{"Name: foo", "Image: golang:1.24.1-bookworm"},
 		})
 	})
 }
--- shared/README.md
+++ shared/README.md
@@ -2,4 +2,30 @@
 
 These are re-useable chunks of contexts or tasks that can be imported via the URL into other projects to avoid repetition.
 
-You can easily see imported contexts/pipelines/tasks by running the `eirctl list` command.
+You can easily see imported contexts/pipelines/tasks by running the `eirctl list` and `eirctl show <task|pipeline|context>` command.
+
+## TF Shared Example
+
+Reusable tasks and contexts defined in the [shared/infra/tf.yaml](./infra/tf.yaml) can be used like this in a consumer eirctl yaml
+
+```yaml
+import:
+  - https://raw.githubusercontent.com/Ensono/eirctl/e0e0d9abc998f58930d1a2fb371b17456e8b3230/shared/infra/tf.yaml
+
+pipelines:
+  run:infra:workspace:
+    - task: eirctl:tf:init
+      env:
+        TF_DIR: aws/infra
+    - task: eirctl:tf:plan:workspace
+      depends_on:
+        - eirctl:tf:init
+      env:
+        TF_DIR: aws/infra
+        WORKSPACE: test1
+    - task: eirctl:tf:apply:workspace
+      depends_on:
+        - eirctl:tf:plan:workspace
+      env:
+        TF_DIR: aws/infra
+```
