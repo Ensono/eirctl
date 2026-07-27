3cd29b7f788ba7c4f5e54a04fc2411fa346a571a
Richard Slater <richard.slater@ensono.com>
2026-07-27 11:38:08 +0100

    refactor(runner): group command compiler inputs

--- runner/compiler.go
+++ runner/compiler.go
@@ -36,18 +36,18 @@ func (tc *TaskCompiler) CompileTask(t *task.Task, executionContext *ExecutionCon
 	for _, variant := range t.GetVariations() {
 		// each command in the array needs compiling
 		for _, command := range t.Commands {
-			j, err := tc.compileCommand(
-				t,
-				command,
-				executionContext,
-				t.Dir,
-				t.Timeout,
-				stdin,
-				stdout,
-				stderr,
-				env.Merge(variables.FromMap(variant)),
-				vars,
-			)
+			j, err := tc.compileCommand(compileCommandInput{
+				task:         t,
+				command:      command,
+				executionCtx: executionContext,
+				dir:          t.Dir,
+				timeout:      t.Timeout,
+				stdin:        stdin,
+				stdout:       stdout,
+				stderr:       stderr,
+				env:          env.Merge(variables.FromMap(variant)),
+				vars:         vars,
+			})
 			if err != nil {
 				return nil, err
 			}
@@ -85,53 +85,57 @@ func appendJob(head, tail, job *Job) (*Job, *Job) {
 	return head, job
 }
 
+type compileCommandInput struct {
+	task         *task.Task
+	command      string
+	executionCtx *ExecutionContext
+	dir          string
+	timeout      *time.Duration
+	stdin        io.Reader
+	stdout       io.Writer
+	stderr       io.Writer
+	env          *variables.Variables
+	vars         *variables.Variables
+}
+
 // CompileCommand compiles command into Job
-func (tc *TaskCompiler) compileCommand(
-	task *task.Task,
-	command string,
-	executionCtx *ExecutionContext,
-	dir string,
-	timeout *time.Duration,
-	stdin io.Reader,
-	stdout, stderr io.Writer,
-	env, vars *variables.Variables,
-) (*Job, error) {
+func (tc *TaskCompiler) compileCommand(input compileCommandInput) (*Job, error) {
 	j := &Job{
-		Timeout: timeout,
-		Env:     env,
-		Stdin:   stdin,
-		Stdout:  stdout,
-		Stderr:  stderr,
-		Vars:    tc.variables.Merge(vars),
+		Timeout: input.timeout,
+		Env:     input.env,
+		Stdin:   input.stdin,
+		Stdout:  input.stdout,
+		Stderr:  input.stderr,
+		Vars:    tc.variables.Merge(input.vars),
 	}
 
 	// Look at the executable details and check if the command is running `docker` determine if an Envfile is being generated
 	// If it has then check to see if the args contains the --env-file flag and if it does modify the path to the envfile
 	// if it does not then add the --env-file flag to the args array
-	if executionCtx.Envfile != nil { // && executionCtx.Executable.IsContainer
+	if input.executionCtx.Envfile != nil { // && executionCtx.Executable.IsContainer
 		// generate the envfile with supplied env only
-		err := executionCtx.ProcessEnvfile(env)
+		err := input.executionCtx.ProcessEnvfile(input.env)
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	c := []string{command}
-	if executionCtx.Executable != nil {
-		c = []string{executionCtx.Executable.Bin}
-		c = append(c, executionCtx.Executable.Args...)
-		c = append(c, fmt.Sprintf("%s%s%s", executionCtx.Quote, command, executionCtx.Quote))
+	c := []string{input.command}
+	if input.executionCtx.Executable != nil {
+		c = []string{input.executionCtx.Executable.Bin}
+		c = append(c, input.executionCtx.Executable.Args...)
+		c = append(c, fmt.Sprintf("%s%s%s", input.executionCtx.Quote, input.command, input.executionCtx.Quote))
 	}
 
 	j.Command = strings.Join(c, " ")
-	j.Env = executionCtx.Env
+	j.Env = input.executionCtx.Env
 	logrus.Debugf("command: %s", j.Command)
 
 	var err error
-	if dir != "" {
-		j.Dir = dir
-	} else if executionCtx.Dir != "" {
-		j.Dir = executionCtx.Dir
+	if input.dir != "" {
+		j.Dir = input.dir
+	} else if input.executionCtx.Dir != "" {
+		j.Dir = input.executionCtx.Dir
 	}
 
 	j.Dir, err = utils.ParseTemplate(j.Dir, j.Vars.Map(), j.Env.Map())
@@ -143,8 +147,8 @@ func (tc *TaskCompiler) compileCommand(
 	//
 	// NOTE: This could also be a compile time check - but it would preclude any dynamically set environment
 	// variables to be checked
-	if task.Required != nil && task.Required.HasRequired() {
-		if err := task.Required.Check(j.Env.Merge(task.Env), j.Vars); err != nil {
+	if input.task.Required != nil && input.task.Required.HasRequired() {
+		if err := input.task.Required.Check(j.Env.Merge(input.task.Env), j.Vars); err != nil {
 			return nil, err
 		}
 	}
