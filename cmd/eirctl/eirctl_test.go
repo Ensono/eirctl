5727b6a723e41af216d51818e4f2abd3a45c0b1a
Richard Slater <richard.slater@ensono.com>
2026-07-27 11:26:21 +0100

    test(eirctl): split command test helper

--- cmd/eirctl/eirctl_test.go
+++ cmd/eirctl/eirctl_test.go
@@ -39,54 +39,62 @@ func (o mockOsFsOps) Create(name string) (io.Writer, error) {
 
 func cmdRunTestHelper(t *testing.T, testInput *cmdRunTestInput) {
 	t.Helper()
-	ctx := context.TODO()
+	err, logOutput, errOutputLength := executeCommandTest(t, testInput)
+	if err != nil {
+		assertCommandError(t, testInput, err)
+		return
+	}
 
-	if testInput.ctx != nil {
-		ctx = testInput.ctx
+	assertCommandOutput(t, testInput, logOutput, errOutputLength)
+}
+
+func executeCommandTest(t *testing.T, testInput *cmdRunTestInput) (error, string, int) {
+	t.Helper()
+	ctx := testInput.ctx
+	if ctx == nil {
+		ctx = context.TODO()
 	}
 
 	logOut := output.NewSafeWriter(&bytes.Buffer{})
 	logErr := output.NewSafeWriter(&bytes.Buffer{})
-
 	cmd := eirctlCmd.NewEirCtlCmd(ctx, logOut, logErr)
 	os.Args = append([]string{os.Args[0]}, testInput.args...)
 
 	cmd.Cmd.SetArgs(testInput.args)
 	errOut := output.NewSafeWriter(&bytes.Buffer{})
-	stdOut := output.NewSafeWriter(&bytes.Buffer{})
 	cmd.Cmd.SetErr(errOut)
-	cmd.Cmd.SetOut(stdOut)
+	cmd.Cmd.SetOut(output.NewSafeWriter(&bytes.Buffer{}))
 	cmd.OsFsOps = mockOsFsOps{}
-
 	if err := cmd.InitCommand(eirctlCmd.WithSubCommands()...); err != nil {
 		t.Fatal(err)
 	}
 
-	if err := cmd.Execute(); err != nil {
-		if testInput.errored {
-			if len(testInput.output) > 0 {
-				for _, v := range testInput.output {
-					if !(strings.Contains(err.Error(), v)) {
-						t.Errorf("\nerror: %s\n\ndoes not contain: %v\n", err.Error(), v)
-					}
-				}
-			}
-			return
-		}
+	return cmd.Execute(), logOut.String(), errOut.Len()
+}
+
+func assertCommandError(t *testing.T, testInput *cmdRunTestInput, err error) {
+	t.Helper()
+	if !testInput.errored {
 		t.Fatalf("\ngot: %v\nwanted <nil>\n", err)
 	}
-
-	if testInput.errored && errOut.Len() < 1 {
-		t.Errorf("\ngot: nil\nwanted an error to be thrown")
-	}
-	if len(testInput.output) > 0 {
-		for _, v := range testInput.output {
-			if !strings.Contains(logOut.String(), v) {
-				t.Errorf("\ngot: %s\vnot found in: %v", logOut.String(), v)
-			}
+	for _, expectedOutput := range testInput.output {
+		if !strings.Contains(err.Error(), expectedOutput) {
+			t.Errorf("\nerror: %s\n\ndoes not contain: %v\n", err.Error(), expectedOutput)
 		}
 	}
-	if testInput.exactOutput != "" && logOut.String() != testInput.exactOutput {
-		t.Errorf("output mismatch\ngot: %s\n\nwanted: %s", logOut.String(), testInput.exactOutput)
+}
+
+func assertCommandOutput(t *testing.T, testInput *cmdRunTestInput, logOutput string, errOutputLength int) {
+	t.Helper()
+	if testInput.errored && errOutputLength < 1 {
+		t.Errorf("\ngot: nil\nwanted an error to be thrown")
+	}
+	for _, expectedOutput := range testInput.output {
+		if !strings.Contains(logOutput, expectedOutput) {
+			t.Errorf("\ngot: %s\vnot found in: %v", logOutput, expectedOutput)
+		}
+	}
+	if testInput.exactOutput != "" && logOutput != testInput.exactOutput {
+		t.Errorf("output mismatch\ngot: %s\n\nwanted: %s", logOutput, testInput.exactOutput)
 	}
 }
